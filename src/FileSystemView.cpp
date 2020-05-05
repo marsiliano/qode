ac0ad83f8a03918b4fc57ba2c5710123f77c2146
Federico Guerinoni <guerinoni.federico@gmail.com>
2020-05-05 10:20:45 +0200

    Show only "name" column of filesystem treeview
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/FileSystemView.cpp
+++ src/FileSystemView.cpp
@@ -1,11 +1,18 @@
 #include "FileSystemView.hpp"
 
+#include <QHeaderView>
+
 FileSystemView::FileSystemView()
 {
     setFeatures(features() & ~QDockWidget::DockWidgetClosable);
     setFeatures(features() & ~QDockWidget::DockWidgetFloatable);
 
     m_tree.setModel(&m_model);
+    auto headerView = m_tree.header();
+    for (auto i = 1; i < headerView->count(); ++i) {
+        headerView->setSectionHidden(i, true);
+    }
+
     setWidget(&m_tree);
 }
 

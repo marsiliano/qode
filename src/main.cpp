c20c0b3398972238b5aeb1a7ee09e09e8cdc9340
Federico Guerinoni <guerinoni.federico@gmail.com>
2020-05-06 11:34:54 +0200

    Add DarkMode
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,33 @@
 #include <QApplication>
 #include <QCommandLineParser>
 #include <QFile>
+#include <QStyleFactory>
 #include <QtDebug>
+
+void prepareStyle()
+{
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+    QPalette darkPalette;
+    QColor darkColor = QColor(50, 50, 50);
+    QColor disabledColor = QColor(127, 127, 127);
+    darkPalette.setColor(QPalette::Window, darkColor);
+    darkPalette.setColor(QPalette::WindowText, Qt::gray);
+    darkPalette.setColor(QPalette::Base, QColor(30, 30, 30));
+    darkPalette.setColor(QPalette::AlternateBase, darkColor);
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Disabled, QPalette::Text, disabledColor);
+    darkPalette.setColor(QPalette::Button, darkColor);
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, disabledColor);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, disabledColor);
+    qApp->setPalette(darkPalette);
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +37,7 @@ int main(int argc, char *argv[])
     a.setApplicationName(QStringLiteral("qcode"));
     a.setApplicationVersion(QStringLiteral("0.0.1"));
     a.setApplicationDisplayName(QStringLiteral("qcode"));
+    prepareStyle();
 
     QCommandLineParser parser;
     parser.setApplicationDescription(QObject::tr("Simple editor for general purpose use."));
